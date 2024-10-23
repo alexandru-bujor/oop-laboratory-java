1d7ff780de31d3e9f01fbe345385180001339fec
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2024-10-23 16:29:22 +0300

    This is the final version for lab0. For this part I used if else solution, and if it is HIARY it goes to Marvel, etc.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,68 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        File inputFile = new File("src/main/resources/test-input.json");
+        JsonNode data = mapper.readTree(inputFile).get("data");
+
+        Universe starWars = new Universe("starwars", new ArrayList<>());
+        Universe hitchhikers = new Universe("hitchhikers", new ArrayList<>());
+        Universe marvel = new Universe("marvel", new ArrayList<>());
+        Universe rings = new Universe("rings", new ArrayList<>());
+
+        for (JsonNode entry : data) {
+            int id = entry.get("id").asInt();
+            boolean isHumanoid = entry.has("isHumanoid") && entry.get("isHumanoid").asBoolean();
+            String planet = entry.has("planet") ? entry.get("planet").asText() : "Unknown Planet";
+            List<String> traits = new ArrayList<>();
+            if (entry.has("traits")) {
+                for (JsonNode trait : entry.get("traits")) {
+                    traits.add(trait.asText());
+                }
+            }
+
+            if (isHumanoid) {
+                if (planet.equalsIgnoreCase("Earth")) {
+                    marvel.individuals().add(entry.toString());
+                    System.out.println("Added humanoid from Earth to Marvel: " + entry);
+                } else if (planet.equalsIgnoreCase("Asgard")) {
+                    rings.individuals().add(entry.toString());
+                    System.out.println("Added humanoid from Asgard to Rings: " + entry);
+                } else {
+                    hitchhikers.individuals().add(entry.toString());
+                    System.out.println("Added humanoid to Hitchhiker's Universe: " + entry);
+                }
+            } else {
+                if (traits.contains("HAIRY") || traits.contains("TALL")) {
+                    starWars.individuals().add(entry.toString());
+                    System.out.println("Added non-humanoid (HAIRY/TALL) to Star Wars: " + entry);
+                } else {
+                    hitchhikers.individuals().add(entry.toString());
+                    System.out.println("Added non-humanoid to Hitchhiker's Universe: " + entry);
+                }
+            }
         }
+
+        mapper.writeValue(new File("src/main/resources/output/starwars.json"), starWars);
+        mapper.writeValue(new File("src/main/resources/output/hitchhikers.json"), hitchhikers);
+        mapper.writeValue(new File("src/main/resources/output/marvel.json"), marvel);
+        mapper.writeValue(new File("src/main/resources/output/rings.json"), rings);
+
+        System.out.println("\nClassification Summary:");
+        System.out.println("Star Wars Universe: " + starWars.individuals().size() + " aliens");
+        System.out.println("Hitchhiker's Universe: " + hitchhikers.individuals().size() + " aliens");
+        System.out.println("Marvel Universe: " + marvel.individuals().size() + " aliens");
+        System.out.println("Rings Universe: " + rings.individuals().size() + " aliens");
     }
 }
+
+record Universe(String name, List<String> individuals) {}
