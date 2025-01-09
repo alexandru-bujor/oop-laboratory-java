862aeabb8e0d2c0bd342c979c5198a61fa1714cb
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2025-01-10 00:23:26 +0200

    Created the action menu for the Main program with the posibility to select different options like task1/2/3/4 or exit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,48 @@
 package org.example;
 import org.example.task1.Display;
+import java.util.Scanner;
 
 public class Main {
-    static void myMethod() {
-        System.out.println("I just got executed!");
-    }
+
 
     public static void main(String[] args) {
-        Display d1 = new Display(1080, 1920, 96.0f, "Samsung");
-        Display d2 = new Display(1440, 900, 67.0f, "AOC");
 
-        d1.compareSize(d2);
-        d1.compareSharpness(d2);
-        d1.compareWithMonitor(d2);
+        do {
+            System.out.println("Select an option: \n");
+            System.out.println("1-Task 1: \n");
+            System.out.println("2-Task 2: \n");
+            System.out.println("3-Task 3: \n");
+            System.out.println("4-Task 4: \n");
+            System.out.println("5-exit: \n");
+            Scanner scanner = new Scanner(System.in);
+            String inputMain = scanner.nextLine();
+            System.out.println("----------------------------");
+
+            switch (inputMain) {
+                case "1":
+                    System.out.println("Starting task 1: \n");
+
+                    Display d1 = new Display(1080, 1920, 96.0f, "Samsung");
+                    Display d2 = new Display(1440, 900, 67.0f, "AOC");
+                    d1.compareSize(d2);
+                    d1.compareSharpness(d2);
+                    d1.compareWithMonitor(d2);
+                    break;
+
+                case "2":
+                    System.out.println("Starting task 2: \n");
+                    break;
+
+                case "5":
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+            }
+        } while (true);
+
+
+
+
 
 
     }
